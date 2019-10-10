9a4cf75c55a5c10e0dc803621ec8dcccf6aabd78
Giacomo Terreni <giacomo.terreni@gmail.com>
2019-10-10 11:13:54 +0200

    initial release

---
+++ src/FilesystemServiceProvider.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Gecche\Cupparis\Filesystem;
+
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Arr;
+use Illuminate\Support\ServiceProvider;
+use Symfony\Component\HttpFoundation\File\MimeType\MimeTypeGuesser;
+
+class FilesystemServiceProvider extends ServiceProvider
+{
+
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        Filesystem::macro('deleteFiles', function ($pattern,$flags = 0) {
+            $this->delete($this->glob($pattern,$flags));
+        });
+
+        Filesystem::macro('mimeFromGuesser', function ($path) {
+            $guesser = MimeTypeGuesser::getInstance();
+
+            try {
+                $mimetype = $guesser->guess($path);
+            } catch (\Exception $e) {
+                return false;
+            }
+
+            return $mimetype;
+        });
+
+        Filesystem::macro('getIconaMime', function ($path, $iconeMimesArray = [], $default = 'default.png') {
+
+            $mimetype = $this->mimeFromGuesser($path);
+            if ($mimetype === false) {
+                return $default;
+            }
+            if (is_array($mimetype)) {
+                $mimetype = current($mimetype);
+            }
+
+            return Arr::get($iconeMimesArray, $mimetype, $default);
+
+        });
+
+
+    }
+
+}
